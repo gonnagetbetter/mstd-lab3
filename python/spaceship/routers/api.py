ffddfa80e9cfc1e6033fb7243a81afbbbd4e5e3d
gonnagetbetter <denissss906@gmail.com>
2023-04-02 21:52:40 +0300

    feature: added matrix multiplier

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,8 +1,18 @@
+import numpy
 from fastapi import APIRouter
 
 router = APIRouter()
 
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+    product = numpy.dot(matrix_a, matrix_b)
 
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+    result = {
+        "matrix_a": matrix_a.tolist(),
+        "matrix_b": matrix_b.tolist(),
+        "product": product.tolist()
+    }
+
+    return result
